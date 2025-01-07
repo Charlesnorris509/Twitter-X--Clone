94b94d79a800d01b79ccd17103e852601b51ad33
Charles Norris <59802926+Charlesnorris509@users.noreply.github.com>
2025-01-07 14:30:33 -0500

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,8 +1,18 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/tailwind.css';
+import './style/Index.css';
 import App from './App';
 
+/**
+ * Entry point of the React application.
+ * This file is responsible for rendering the App component into the DOM.
+ *
+ * Features:
+ * - Imports global CSS styles from Tailwind.
+ * - Utilizes React's StrictMode for highlighting potential problems.
+ * - Renders the main App component, which sets up routing and navigation.
+ */
 ReactDOM.render(
   <React.StrictMode>
     <App />
