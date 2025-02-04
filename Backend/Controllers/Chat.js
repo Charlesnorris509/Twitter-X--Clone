48f8903fa67b5bbf6f481a12d05c278bd79d721a
Charles Norris <59802926+Charlesnorris509@users.noreply.github.com>
2025-02-04 13:10:10 -0500

    Create Chat.js

---
+++ Backend/Controllers/Chat.js
@@ -0,0 +1,31 @@
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+
+    socket.on('newMessage', (message) => {
+        io.emit('message', message);
+    });
+
+    socket.on('newNotification', (notification) => {
+        io.emit('notification', notification);
+    });
+
+    socket.on('typing', (userId) => {
+    io.emit('typing', userId);
+    });
+
+    socket.on('stopTyping', (userId) => {
+    io.emit('stopTyping', userId);
+    });
+
+    socket.on('updateLastSeen', (userId) => {
+    io.emit('lastSeen', UserId);
+    });
+});
+
+      
